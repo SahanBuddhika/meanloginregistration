576bb939b4df81a4eba20cc7e563b65e59d2cbab
SahanBuddhika <sahan.bu.1994@gmail.com>
2019-05-10 10:27:10 +0530

    create register.component.ts page

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {AuthenticationService , TokenPayload} from '../authentication.service'
 import {Router} from '@angular/router';
 
+
 @Component({
-  templateUrl: './login.component.html'
+  templateUrl: './register.component.html'
 })
 
-export class LoginComponent{
+export class RegisterComponent{
   credentials:TokenPayload = {
     _id:'',
     first_name:'',
@@ -17,8 +18,8 @@ export class LoginComponent{
 
   constructor(private auth: AuthenticationServeice,private router: Router){}
 
-  login(){
-    this.auth.login(this.credentials).subscribe(
+  register(){
+    this.auth.register(this.credentials).subscribe(
       ()={
         this.router.navigateByUrl('/profile')
       },
